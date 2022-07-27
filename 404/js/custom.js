a66f4475aee00a9ca5b7e3a1c67da7d43e7bf789
FlechazoCLF <flechazo.clf@gmail.com>
2022-07-27 19:43:40 +0800

    Site updated: 2022-07-27 19:43:37

--- 404/js/custom.js
+++ 404/js/custom.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 25 2022 20:24:57 GMT+0800 (GMT+08:00)
+// build time:Wed Jul 27 2022 19:43:06 GMT+0800 (GMT+08:00)
 (function(n){"use strict";var i={main_fun:function(){var n=new countUp("error-link",10,404,0,5);window.onload=function(){n.start()}},initialization:function(){i.main_fun()}};n(document).ready(function(){i.main_fun()})})(jQuery);
 //rebuild by hrmmi 
